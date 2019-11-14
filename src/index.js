c6cdba6a053e41658d852f9591b51a6cb0cfdfbb
dimaeromin97 <dimaeromin97@gmail.com>
2019-11-14 09:45:46 +0200

    Fixed path file split separator

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const aliasImporter = (aliases, options = {}) => {
   return (url, prev, done) => {
     let aliasFound = false;
     aliasesDetails.forEach(aliasDetails => {
-      if (!aliasFound && url.split(path.sep)[0] === aliasDetails.alias) {
+      if (!aliasFound && url.split(new RegExp( `${ path.sep }|/`.replace( "\\", "\\\\" ), "g" ))[0] === aliasDetails.alias) {
         aliasFound = true;
         done({
           file: url.replace(
